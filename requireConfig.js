336e875c1026a2a550f9a0e37eed87e40df2fcd5
curran <curran.kelleher@gmail.com>
2014-05-02 15:04:15 -0400

    Cleaned up directory structure, files

--- requireConfig.js
+++
@@ -1,8 +0,0 @@
-// The configuration for Require.js
-// Curran Kelleher 4/21/2014
-require.config({
-  baseUrl: 'src',
-  paths: {
-    _: '../lib/lodash/dist/lodash.min'
-  }
-});
--- tests/requireConfig.js
+++ tests/requireConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   baseUrl: '.',
   paths: {
     udc: 'dist/udc',
-    _: 'lib/lodash/dist/lodash.min'
+    _: 'bower_components/lodash/dist/lodash.min'
   }
 };
