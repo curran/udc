3e2b8a288cc6585e2d1d362fbd2613c073ff495b
curran <curran.kelleher@gmail.com>
2014-06-22 13:32:03 -0400

    Refactored thesaurus index to use member id rather than (dimension, codelist, code) tuples. Polished documentation.

--- src/thesaurus.js
+++ src/thesaurus.js
@@ -6,15 +6,33 @@ define(['getMember', 'getCell'], function (getMember, getCell) {
   // A Thesaurus is constructed from many datasets.
   return function Thesaurus (datasets) {
     
-    // index[dimension][codelist][code] = equivalenceClass
-    // equivalenceClass[codelist] = Member
+    // The index maps Members to their equivalence classes.
+    //
+    //  * `index[memberId] = equivalenceClass`
+    //  * `equivalenceClass[codelist] = Member`
     var index = {},
 
-        // canonicalCodelists[dimension] = codelist
+        // Stores the set of codelists used by each dimension.
+        // Used for determining canonical codelists later.
+        //
+        //  * `codelistsByDimension[dimension][codelist] = true`
+        codelistsByDimension = {},
+
+        // Stores canonical codelists for each dimension.
+        //
+        //  * `canonicalCodelists[dimension] = codelist`
         canonicalCodelists = {};
 
     // Build the index.
     datasets.forEach(function (dataset) {
+
+      // For each dimensionColumn,
+      dataset.dimensionColumns.forEach(function (d) {
+        
+        // store its codelist.
+        var dimensionIndex = codelistsByDimension[d.dimension] || (codelistsByDimension[d.dimension] = {});
+        dimensionIndex[d.codeList] = true;
+      });
 
       // For each row of the dataset,
       dataset.rows.forEach(function (row) {
@@ -30,60 +48,40 @@ define(['getMember', 'getCell'], function (getMember, getCell) {
           var dimension = d.dimension,
               codelist = d.codeList,
               code = row[d.column],
-              member = getMember(dimension, codelist, code),
+              member = getMember(dimension, codelist, code);
 
-              // Get or create the index bucket for the Member object.
-              dimensionIndex = index[dimension] || (index[dimension] = {}),
-              codelistIndex = dimensionIndex[codelist] || (dimensionIndex[codelist] = {});
-
-          // Add the Member to the equivalence class.
+          // add the Member to the equivalence class, and
           equivalenceClass[codelist] = member;
 
-          // Add the equivalence class to the Thesaurus index.
-          codelistIndex[code] = equivalenceClass;
+          // add the equivalence class to the index.
+          index[member.id] = equivalenceClass;
 
         });
       });
     });
 
-    // Derive canonical code lists for each dimension.
-    var dimensions = Object.keys(index);
-    dimensions.forEach(function (dimension) {
-      var codelists = Object.keys(index[dimension]);
+    // For each dimension,
+    Object.keys(codelistsByDimension).forEach(function (dimension) {
+      var codelists = Object.keys(codelistsByDimension[dimension]);
+
+      // derive the canonical code lists by first sorting the codelists
+      // for this dimension, then choosing the first one in sorted order.
       canonicalCodelists[dimension] = codelists.sort()[0];
     });
 
     // Translates the given member to the given code list.
     function translate(member, codelist){
-      return index[member.dimension][member.codeList][member.code][codelist];
+      return index[member.id][codelist];
     }
 
     // Translates the given member to the canonical code list for its dimension.
     function canonicalizeMember(member){
-      var dimensionIndex = index[member.dimension],
-          codelistIndex,
-          codelist;
-      if(dimensionIndex) {
-        codelistIndex = dimensionIndex[member.codeList];
-        if(codelistIndex.hasOwnProperty(member.code)) {
-          codelist = canonicalCodelists[member.dimension];
-          return codelistIndex[member.code][codelist];
-
-          // TODO find a case where this is necessary
-          //var equivalenceClass = codelistIndex[member.code],
-          //    codelists = canonicalCodelists[member.dimension],
-          //    codelist,
-          //    i;
-          //console.log(equivalenceClass);
-          //for(i = 0; i < codelists.length; i++) {
-          //  codelist = codelists[i];
-          //  if(equivalenceClass.hasOwnProperty(codelist)) {
-          //    return equivalenceClass[codelist];
-          //  }
-          //}
-        }
+      var equivalenceClass = index[member.id];
+      if(equivalenceClass) {
+        return equivalenceClass[canonicalCodelists[member.dimension]];
+      } else {
+        return member;
       }
-      return member;
     }
 
     // Translates the members of the given cell to the canonical code lists
