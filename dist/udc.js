6c1cc22cd45fa1f5986e36a3259be9ac21a56d76
curran <curran.kelleher@gmail.com>
2014-06-21 20:33:13 -0400

    Refacted createObservation.js out of createCube.js

--- dist/udc.js
+++ dist/udc.js
@@ -94,7 +94,37 @@ define('getCell',['_'], function () {
   */
 });
 
-define('createCube',['_', 'getMember', 'getCell'], function (_, getMember, getCell) {
+define('createObservation',['getMember', 'getCell'], function (getMember, getCell) {
+  // Observation objects contain:
+  //
+  //  * cell: Cell - The Cell defining the domain of this createObservation.
+  //  * values: { measureName -> Number } - An object that maps measures to values.
+  return function createObservation(row, dimensionColumns, measureColumns) {
+
+    // Look up the cell corresponding to the unique set of members
+    // expressed in the dimension columns of the given row.
+    var cell = getCell(dimensionColumns.map(function (d) {
+          var dimension = d.dimension,
+              codeList = d.codeList,
+              code = row[d.column];
+          return getMember(dimension, codeList, code);
+        })),
+        values = {};
+
+    // Populate the `values` object, which maps measures
+    // to numeric values adjusted to the scale of each measureColumn.
+    measureColumns.forEach(function (d) {
+      values[d.measure] = row[d.column] * d.scale;
+    });
+
+    return {
+      cell: cell,
+      values: values
+    };
+  };
+});
+
+define('createCube',['_', 'createObservation'], function (_, createObservation) {
 
   // Creates a cube from the given `dataset` object, where
   //
@@ -127,34 +157,6 @@ define('createCube',['_', 'getMember', 'getCell'], function (_, getMember, getCe
       })
     };
   };
-
-  // Observation objects contain:
-  //
-  //  * cell: Cell - The Cell defining the domain of this createObservation.
-  //  * values: { measureName -> Number } - An object that maps measures to values.
-  function createObservation(row, dimensionColumns, measureColumns) {
-
-    // Look up the cell corresponding to the unique set of members
-    // expressed in the dimension columns of the given row.
-    var cell = getCell(dimensionColumns.map(function (d) {
-          var dimension = d.dimension,
-              codeList = d.codeList,
-              code = row[d.column];
-          return getMember(dimension, codeList, code);
-        })),
-        values = {};
-
-    // Populate the `values` object, which maps measures
-    // to numeric values adjusted to the scale of each measureColumn.
-    measureColumns.forEach(function (d) {
-      values[d.measure] = row[d.column] * d.scale;
-    });
-
-    return {
-      cell: cell,
-      values: values
-    };
-  }
 });
 
 define('cubeIndex',[], function () {
--- src/createCube.js
+++ src/createCube.js
@@ -1,4 +1,4 @@
-define(['_', 'getMember', 'getCell'], function (_, getMember, getCell) {
+define(['_', 'createObservation'], function (_, createObservation) {
 
   // Creates a cube from the given `dataset` object, where
   //
@@ -31,32 +31,4 @@ define(['_', 'getMember', 'getCell'], function (_, getMember, getCell) {
       })
     };
   };
-
-  // Observation objects contain:
-  //
-  //  * cell: Cell - The Cell defining the domain of this createObservation.
-  //  * values: { measureName -> Number } - An object that maps measures to values.
-  function createObservation(row, dimensionColumns, measureColumns) {
-
-    // Look up the cell corresponding to the unique set of members
-    // expressed in the dimension columns of the given row.
-    var cell = getCell(dimensionColumns.map(function (d) {
-          var dimension = d.dimension,
-              codeList = d.codeList,
-              code = row[d.column];
-          return getMember(dimension, codeList, code);
-        })),
-        values = {};
-
-    // Populate the `values` object, which maps measures
-    // to numeric values adjusted to the scale of each measureColumn.
-    measureColumns.forEach(function (d) {
-      values[d.measure] = row[d.column] * d.scale;
-    });
-
-    return {
-      cell: cell,
-      values: values
-    };
-  }
 });
